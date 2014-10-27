54874f142f68b8c1f7f78da9eda0ec1fa061223c
Sven Brettschneider <brettschneider.sven@gmail.com>
2014-10-27 13:01:18 +0100

    falend begin timer enzo

--- GoudKoorts/Presentation/InputView.cs
+++ GoudKoorts/Presentation/InputView.cs
@@ -8,5 +8,23 @@ namespace GoudKoorts.Presentation
 {
     public class InputView
     {
+        public bool WisselInput(){
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Q:
+                        Console.WriteLine("You pressed F1!");
+                        return true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
--- GoudKoorts/Process/Controller.cs
+++ GoudKoorts/Process/Controller.cs
@@ -5,19 +5,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace GoudKoorts.Process
 {
     public class Controller
     {
-        public Spel Spel { get; set; }
-        public OutputView OutputView { get; set; }
+        private Spel spel;
+        private OutputView outputView;
+        private InputView inputView;
+        private Timer timer;
+        private int stapIndex;
+
+        //aantal seconden per stapje in beurt
+        private static int timeInSecondsMax = 1000;
+        private int timeInSecondsNow = timeInSecondsMax;
+
         public Controller()
         {
-            Spel = new Spel();
-            OutputView = new OutputView();
-            OutputView.TekenWereld(Spel);
-            Console.ReadKey();
+            timer = new Timer(timeInSecondsMax);
+            timer.Elapsed += DoeBeurt;
+            timer.Enabled = true;
+            
+            spel = new Spel();
+            inputView = new InputView();
+            outputView = new OutputView();
+            outputView.TekenWereld(spel);
+
+            SpeelSpel();
+        }
+
+        public void SpeelSpel()
+        {
+            bool print = false;
+            while (true)
+            {
+                if (inputView.WisselInput())
+                {
+                    print = !print;
+                }
+                if (print)
+                {
+
+                Console.WriteLine("blabla");
+                }
+                
+            } 
+        }
+
+
+        private void DoeBeurt(Object source, ElapsedEventArgs e)
+        {
+
+            if (stapIndex == 4)
+            {
+                stapIndex = 0;
+            }
+            else
+            {
+                stapIndex++;
+            }
+
+        }
+
+        public void reduceTime()
+        {
+            timer.Interval = timer.Interval * 0.80;
         }
     }
 }
