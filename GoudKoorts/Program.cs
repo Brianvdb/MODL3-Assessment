9590c51808b85df48780959cf35ea7a1db821eed
Brian <bcf.vandenbroek@student.avans.nl>
2014-10-29 09:17:53 +0100

    onnodige code weggehaald

--- GoudKoorts/Program.cs
+++ GoudKoorts/Program.cs
@@ -1,5 +1,4 @@
-﻿using GoudKoorts.Domain;
-using GoudKoorts.Process;
+﻿using GoudKoorts.Process;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +11,6 @@ namespace GoudKoorts
     {
         static void Main(string[] args)
         {
-            Spel spel = new Spel();
-
-            Loods l2 = spel.Loodsen[1];
-
-            Kar kar = new Kar(l2);
-            spel.Karren.AddLast(kar);
-
-            spel.Wissels[1].Switch();
-            spel.Wissels[3].Switch();
-
-            for (int i = 0; i < 50; i++)
-            {
-                spel.VerplaatsKarren();
-
-                Console.WriteLine(kar.Positie);
-            }
-
-
             new Controller();
         }
     }
