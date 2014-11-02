a86c7ec7500d49627b77f6d769114bd68f6838e2
Sven Brettschneider <brettschneider.sven@gmail.com>
2014-11-02 11:31:06 +0100

    kar bots op einde gefixed

--- GoudKoorts/Domain/Kar.cs
+++ GoudKoorts/Domain/Kar.cs
@@ -24,6 +24,7 @@ namespace GoudKoorts.Domain
         {
             if (Positie.Volgende == null)
             {
+                Positie.VerwijderKar();
                 return null;
             }
 
